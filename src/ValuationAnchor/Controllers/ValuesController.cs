8e027c9eedc8d88194b275c35fd807c0e7969cf0
54480 <54480@DESKTOP-EG1K0PM>
2019-03-28 17:45:04 +0800

    cer

--- src/ValuationAnchor/Controllers/ValuesController.cs
+++ src/ValuationAnchor/Controllers/ValuesController.cs
@@ -8,14 +8,15 @@ using Microsoft.AspNetCore.Mvc;
 using System.IO;
 using Microsoft.Extensions.Logging;
 using ValuationAnchor.Helpers;
+using System.Linq;
+using System.Security.Cryptography;
+using Newtonsoft.Json;
 
 namespace ValuationAnchor.Controllers
 {
     [Route("api/[controller]/[action]")]
     public class ValuesController : BaseController
     {
-
-
         private ILogger<dynamic> _logger;
 
         private string _msg = "请求成功";
@@ -28,10 +29,18 @@ namespace ValuationAnchor.Controllers
         [HttpGet]
         public async Task<string> Get()
         {
-            // return new string[] { "value1", "value2" };
-            var list = await GetWebJson<JsonResult>("http://money.finance.sina.com.cn/q/view/newFLJK.php?param=class");
-            _logger.LogInformation("-----Data");
-            return list;
+            var token = await GetToken();
+            if (string.IsNullOrEmpty(token))
+            {
+                _logger.LogInformation("-----token is null");
+                return null;
+            }
+            else
+            {
+                var list = await GetWebJson<JsonResult>("http://money.finance.sina.com.cn/q/view/newFLJK.php?param=class");
+                _logger.LogInformation("-----Data");
+                return list;
+            }
         }
 
         [HttpGet]
@@ -106,7 +115,7 @@ namespace ValuationAnchor.Controllers
                                 {
                                     RedisHelper.SetToken("BlockList_" + i, gn, TimeSpan.FromSeconds(3600));
                                     i++;
-                                    
+
                                 }
                             }
                         }
@@ -114,8 +123,8 @@ namespace ValuationAnchor.Controllers
                     }
                     catch (Exception ex)
                     {
-                       
-                      
+
+
                         return result;
                     }
                 }
@@ -126,13 +135,93 @@ namespace ValuationAnchor.Controllers
         /// 获取token
         /// </summary>
         /// <returns></returns>
-        public string GetToken()
+        public async static Task<string> GetToken()
         {
-            var result = "";
-            return result;
+            string tokenStr = "";
+            string appId = "201609021530300235";
+            string signKey = "0987654321";
+            string signTime = DateTime.Now.ToString("yyyyMMddHHmmss");
+            string random = new Random().Next(100000, 999999).ToString();
+            string signCode = EncryptWithMD5(appId + signTime + random + signKey);
+            Dictionary<string, string> parm = new Dictionary<string, string>();
+            parm.Add("appId", appId);
+            parm.Add("signTime", signTime);
+            parm.Add("random", random);
+            parm.Add("signCode", signCode);
+            Dictionary<string, string> parmHead = new Dictionary<string, string>();
+            parmHead.Add("loginfo", "{\"USERNAME\":\"\",\"PROJECT\":\"test\",\"PLATFORM\":\"web\",\"IP\":\"192.168.1.1\",\"DEVICENUM\":\"\"}");
+            dynamic token = await AsyncPost<dynamic>("http://localhost:37304/" + "api/Token", parm, parmHead);
+            if (token != null && token.code == 57600)
+            {
+                tokenStr = token.data;
+            }
+            return tokenStr;
         }
 
+        public async static Task<T> AsyncPost<T>(string url, Dictionary<string, string> postPara, Dictionary<string, string> head)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                //添加额外的请求头
+                if (head.Any())
+                {
+                    foreach (var item in head)
+                    {
+                        client.DefaultRequestHeaders.Add(item.Key, item.Value);
+                    }
+                }
+                HttpResponseMessage task = await client.PostAsync(new Uri(url), new FormUrlEncodedContent(postPara));
+                string postStr = task.Content.ReadAsStringAsync().Result;
+                if (string.IsNullOrEmpty(postStr))
+                {
+                    return default(T);
+                }
+                else
+                {
+                    try
+                    {
+                        //解析返回
+                        return JsonStrToObj<T>(postStr);
+                    }
+                    catch (Exception ex)
+                    {
 
-      
+                        return default(T);
+                    }
+                }
+            }
+        }
+
+        public static T JsonStrToObj<T>(string jsonStr)
+        {
+            if (!string.IsNullOrEmpty(jsonStr))
+            {
+                var obj = JsonConvert.DeserializeObject<T>(jsonStr);
+                return obj;
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public static string EncryptWithMD5(string str)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(str);
+
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+
+            return sb.ToString().ToLower();
+        }
     }
 }
